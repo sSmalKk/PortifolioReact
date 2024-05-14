b54b6bb1d87b528674bef21a006211de6e8ab066
Gustavo Smalk <gustav.o.g@hotmail.com>
2024-05-14 15:07:42 -0300

    update

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,21 +10,24 @@ export default function Header({ ...props }) {
     >
       <Text className="text-2xl font-normal tracking-[0.18px] md:text-[22px]">DANTAS</Text>
       <ul className="flex items-center gap-[15px]">
-          <a href="/#services" className="cursor-pointer rounded-[3px] active:text-black-900">
-            <Text className="text-base font-semibold tracking-[0.50px]">
-              SERVIÇOS
-            </Text>
-          </a>
-          <a href="/#projects" className="cursor-pointer rounded-[3px] active:text-black-900">
-            <Text className="text-base font-normal tracking-[0.50px]">
-              PROJETOS
-            </Text>
-          </a>
-          <a href="/#about" className="cursor-pointer rounded-[3px] active:text-black-900">
-            <Text className="text-base font-normal tracking-[0.50px]">
-              SOBRE
-            </Text>
-          </a>
+      <button onClick={() => window.location.href = '/#services'} className="cursor-pointer rounded-[3px] active:text-black-900">
+  <Text className="text-base font-semibold tracking-[0.50px]">
+    SERVIÇOS
+  </Text>
+</button>
+
+<button onClick={() => window.location.href = '/#projects'} className="cursor-pointer rounded-[3px] active:text-black-900">
+  <Text className="text-base font-normal tracking-[0.50px]">
+    PROJETOS
+  </Text>
+</button>
+
+<button onClick={() => window.location.href = '/#about'} className="cursor-pointer rounded-[3px] active:text-black-900">
+  <Text className="text-base font-normal tracking-[0.50px]">
+    SOBRE
+  </Text>
+</button>
+
       </ul>
       <div className="flex w-[22%] justify-between gap-5 md:w-full">
         <div className="flex gap-5">
