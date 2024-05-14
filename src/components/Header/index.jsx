a8112f8143482e018ef0dcd1d9b27795499a6bba
Gustavo Smalk <gustav.o.g@hotmail.com>
2024-05-14 15:15:09 -0300

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,24 +10,23 @@ export default function Header({ ...props }) {
     >
       <Text className="text-2xl font-normal tracking-[0.18px] md:text-[22px]">DANTAS</Text>
       <ul className="flex items-center gap-[15px]">
-      <button onClick={() => window.location.href = '/#services'} className="cursor-pointer rounded-[3px] active:text-black-900">
-  <Text className="text-base font-semibold tracking-[0.50px]">
-    SERVIÇOS
-  </Text>
-</button>
+        <button onClick={() => window.location.href = '/#services'} className="cursor-pointer rounded-[3px] active:text-black-900">
+          <Text className="text-base font-semibold tracking-[0.50px]">
+            SERVIÇOS
+          </Text>
+        </button>
 
-<button onClick={() => window.location.href = '/#projects'} className="cursor-pointer rounded-[3px] active:text-black-900">
-  <Text className="text-base font-normal tracking-[0.50px]">
-    PROJETOS
-  </Text>
-</button>
+        <button onClick={() => window.location.href = '/#projects'} className="cursor-pointer rounded-[3px] active:text-black-900">
+          <Text className="text-base font-normal tracking-[0.50px]">
+            PROJETOS
+          </Text>
+        </button>
 
-<button onClick={() => window.location.href = '/#about'} className="cursor-pointer rounded-[3px] active:text-black-900">
-  <Text className="text-base font-normal tracking-[0.50px]">
-    SOBRE
-  </Text>
-</button>
-
+        <button onClick={() => window.location.href = '/#about'} className="cursor-pointer rounded-[3px] active:text-black-900">
+          <Text className="text-base font-normal tracking-[0.50px]">
+            SOBRE
+          </Text>
+        </button>
       </ul>
       <div className="flex w-[22%] justify-between gap-5 md:w-full">
         <div className="flex gap-5">
@@ -42,7 +41,7 @@ export default function Header({ ...props }) {
             </Button>
           </a>
         </div>
-        <Button href="#contact" className="h-[36px] min-w-[92px] rounded-[18px] border border-solid border-green-400 bg-green-700 px-[15px] text-xs tracking-[0.50px] text-white-A700">
+        <Button onClick={() => window.location.href = '/#contact'} className="h-[36px] min-w-[92px] rounded-[18px] border border-solid border-green-400 bg-green-700 px-[15px] text-xs tracking-[0.50px] text-white-A700">
           CONTATO
         </Button>
       </div>
