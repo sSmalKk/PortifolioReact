fdd5e02627d665e69bd0c8813b0fa0069c2b0476
Gustavo Smalk <gustav.o.g@hotmail.com>
2024-06-03 22:33:42 -0300

    Update index.jsx

--- src/components/Popup/index.jsx
+++ src/components/Popup/index.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 import './style.css';
 import { Img } from "../../components"; // Importe o componente Notification
 
-const Popup = ({onClick, title, subTitle, children, onClose, buttonContent, imageUrl }) => {
+const Popup = ({ onClick, title, subTitle, children, onClose, buttonContent, imageUrl }) => {
   return (
     <div className="popup-overlay">
       <div className="popup-container">
-        <button className="popup-close" onClick={onClose}>x</button>
         <div className="popup-content">
+          <button className="popup-close" onClick={onClose}>x</button>
           <div className="popup-header">
             <h2 className="popup-title">{title}</h2>
             {subTitle && <h3 className="popup-subtitle">{subTitle}</h3>}
