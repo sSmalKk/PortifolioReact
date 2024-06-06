bfbdffcc390ca241c0bec03622c7756a73c47673
Gustavo Smalk <gustav.o.g@hotmail.com>
2024-06-06 18:58:07 -0300

    aaa

--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -4,7 +4,7 @@ import { Text, Heading } from "../components";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import { content } from "../components/content.jsx"
-const languageIndex = localStorage.getItem('languageIndex') || 0;
+const languageIndex = 0;
 
 export default function Page({ title, texts }) {
   return (
